b20f698f063e9056c5ea8f3fe027b1f0472f0df2
ComradeVanti <kingvanti650@gmail.com>
2022-02-20 12:27:08 +0100

    Document methods

--- Runtime/ComponentExt.cs
+++ Runtime/ComponentExt.cs
@@ -7,12 +7,35 @@ namespace ComradeVanti.OptUnity
     public static class ComponentExt
     {
 
+        /// <summary>
+        ///     Attempts to find a component of a specific type on this game-object.
+        ///     Will return none if the component is not found
+        /// </summary>
+        /// <param name="comp">The component relative to which to search</param>
+        /// <typeparam name="TValue">The type of the component to search.</typeparam>
+        /// <returns>An optional which may contain the component</returns>
         public static Opt<TValue> TryGetComponent<TValue>(this Component comp) where TValue : Component =>
             comp.GetComponent<TValue>().ToOpt();
 
+        /// <summary>
+        ///     Attempts to find a component of a specific type on this game-object
+        ///     or any of its children.
+        ///     Will return none if the component is not found
+        /// </summary>
+        /// <param name="comp">The component relative to which to search</param>
+        /// <typeparam name="TValue">The type of the component to search.</typeparam>
+        /// <returns>An optional which may contain the component</returns>
         public static Opt<TValue> TryGetComponentInChildren<TValue>(this Component comp) where TValue : Component =>
             comp.GetComponentInChildren<TValue>().ToOpt();
 
+        /// <summary>
+        ///     Attempts to find a component of a specific type on this game-object
+        ///     or any of its parents.
+        ///     Will return none if the component is not found
+        /// </summary>
+        /// <param name="comp">The component relative to which to search</param>
+        /// <typeparam name="TValue">The type of the component to search.</typeparam>
+        /// <returns>An optional which may contain the component</returns>
         public static Opt<TValue> TryGetComponentInParent<TValue>(this Component comp) where TValue : Component =>
             comp.GetComponentInParent<TValue>().ToOpt();
 
