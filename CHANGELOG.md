174b17b621a91a9c1569beb231a0154ceb8b134f
ComradeVanti <kingvanti650@gmail.com>
2022-02-28 09:39:10 +0100

    Add logic to find component on gameobject

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+﻿# Changelog
+## 1.1.1
+### Added
+- Extension to find component on game-object
+- This changelog
---
+++ Runtime/GameObjectExt.cs
@@ -0,0 +1,22 @@
+﻿using ComradeVanti.CSharpTools;
+using UnityEngine;
+
+namespace ComradeVanti.OptUnity
+{
+
+    public static class GameObjectExt
+    {
+
+        /// <summary>
+        ///     Attempts to find a component of a specific type on this game-object.
+        ///     Will return none if the component is not found
+        /// </summary>
+        /// <param name="comp">The component relative to which to search</param>
+        /// <typeparam name="TValue">The type of the component to search.</typeparam>
+        /// <returns>An optional which may contain the component</returns>
+        public static Opt<TValue> TryGetComponent<TValue>(this GameObject go) where TValue : Component =>
+            go.GetComponent<TValue>().ToOpt();
+
+    }
+
+}
